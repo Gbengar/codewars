bb186675159d0b0a09d3780ad2c52c962b0b1a3a
Apple <oshagbemigbenga@gmail.com>
2022-07-17 23:51:07 +0100

    done

---
+++ js/solution165.js
@@ -0,0 +1,4 @@
+//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+function multipleOfIndex(array) {
+    return array.filter((a,b)=> a % b === 0)
+  }
